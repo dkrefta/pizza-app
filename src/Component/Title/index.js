7401816f542a0599abedb114631f5d9de476fdef
Diego Krefta <diego.krefta@softplan.com.br>
2019-05-22 19:21:49 -0300

    first commit

---
+++ src/Component/Title/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Title = () => <h1>Pizza Calculator</h1>;
+
+export default Title
---
+++ src/lib/calculate-pizzas-needed.js
@@ -0,0 +1,8 @@
+export default function calculatePizzasNeeded(
+    numberOfPeople,
+    slicesPerPerson,
+    slicesPerPizza = 8,
+  ) {
+    return Math.ceil(numberOfPeople * slicesPerPerson / slicesPerPizza);
+  }
+  
